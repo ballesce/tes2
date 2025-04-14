0f7c5e14cffaabc96b302ce7599c9849a4a312e9
Reval Rafifasya <skyzy2630@gmail.com>
2025-04-14 19:42:26 +0700

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      keyframes: {
+        typing: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'white' },
+        },
+      },
+      animation: {
+        typing: 'typing 4s steps(40, end) forwards, blink 0.75s step-end infinite',
+      },
+    },
+  },
+  plugins: [],
+};
